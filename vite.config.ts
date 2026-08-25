3e1c939c42b50d70679fd07d0faba04e0088c271
Ethan <ethanxavier1905@outlook.com>
2026-08-25 16:52:25 +0200

    Nature's Nurture website prototype

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 4173,
+    port: 5173,
+    watch: {
+      ignored: ['**/*.pdf'],
+    },
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
