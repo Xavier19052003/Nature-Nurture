91158b13975fd8f539d6648f9c940a37fdbc92d3
Ethan <ethanxavier1905@outlook.com>
2026-08-25 18:23:26 +0200

    Optimize GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/Nature-Nurture/',
+  publicDir: 'public-production',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
